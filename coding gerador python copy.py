8195a3cf1370ecc08131a77ed6356016266cf374
beispielfields-spec <beispielfields@gmail.com>
2026-06-03 14:46:15 -0300

    Update coding gerador python copy.py

--- coding gerador python copy.py
+++ coding gerador python copy.py
@@ -45,10 +45,11 @@
 \pnucltr\pnstart1\pnindent720\pnhang {\pntxta .}}{\*\pnseclvl3\pndec\pnstart1\pnindent720\pnhang {\pntxta .}}{\*\pnseclvl4\pnlcltr\pnstart1\pnindent720\pnhang {\pntxta )}}{\*\pnseclvl5\pndec\pnstart1\pnindent720\pnhang {\pntxtb (}{\pntxta )}}{\*\pnseclvl6
 \pnlcltr\pnstart1\pnindent720\pnhang {\pntxtb (}{\pntxta )}}{\*\pnseclvl7\pnlcrm\pnstart1\pnindent720\pnhang {\pntxtb (}{\pntxta )}}{\*\pnseclvl8\pnlcltr\pnstart1\pnindent720\pnhang {\pntxtb (}{\pntxta )}}{\*\pnseclvl9\pnlcrm\pnstart1\pnindent720\pnhang 
 {\pntxtb (}{\pntxta )}}\pard\plain \ltrpar\ql \li0\ri0\sa200\sl276\slmult1\widctlpar\wrapdefault\aspalpha\aspnum\faauto\adjustright\rin0\lin0\itap0\pararsid5395146 \rtlch\fcs1 \af31507\afs22\alang1025 \ltrch\fcs0 
-\f31506\fs22\lang1033\langfe1033\cgrid\langnp1033\langfenp1033 {\rtlch\fcs1 \af31507 \ltrch\fcs0 \lang1046\langfe1033\langnp1046\insrsid5395146\charrsid5395146 # -*- coding: utf-8 -*-
+\f31506\fs22\lang1033\langfe1033\cgrid\langnp1033\langfenp1033 {\rtlch\fcs1 \af31507 \ltrch\fcs0 \lang1046\langfe1033\langnp1046\insrsid5395146\charrsid5395146 
+\par # -*- coding: utf-8 -*-
 \par import numpy as np
 \par import matplotlib.pyplot as plt
-\par }{\rtlch\fcs1 \af31507 \ltrch\fcs0 \lang1046\langfe1033\langnp1046\insrsid5395146\charrsid5395146 import pandas as pd
+\par import pandas as pd
 \par 
 \par # --------------------------------------------------
 \par # 1. Leitura dos 12 valores mensais
